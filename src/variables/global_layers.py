65ea782514cbc442d68bd71565529a7c14cdbe29
koentieskens <koen.tieskens@gmail.com>
2026-03-26 10:32:23 -0400

    Add `data_unit` field to `GlobalLayerData` and `GlobalVectorLayerData`, update unit assignments for consistency across layers

--- src/variables/global_layers.py
+++ src/variables/global_layers.py
@@ -4,12 +4,14 @@ from src.variables import variable_template
 @dataclass
 class GlobalLayerData( variable_template.Data):
     """Dataclass to store metadata for a variable. This template will be used to create spatial variables"""
-    unit: str
+
     gcs_path: str
     source: str
     scale: int
     band: int
     bucket: str
+    data_unit: str = None
+    unit: str = 'USD per ha per year'
 
     @classmethod
     def to_dataframe(cls):
@@ -32,6 +34,7 @@ class GlobalLayerData( variable_template.Data):
                     'full_name': var.full_name,
                     'description': getattr(var, 'description', None),
                     'unit': getattr(var, 'unit', None),
+                    'data_unit': getattr(var, 'data_unit', None),
                     'gcs_path': getattr(var, 'gee_path', None),
                     'source': getattr(var, 'source', None),
                     'scale': getattr(var, 'scale', None),
@@ -52,7 +55,7 @@ class GlobalLayer:
         description="""NBS annual opportunity cost based on Busch et al. (2024),
         Costs are summed over a period of 30 years and time-discounted at a rate of 5% annually.""",
         gcs_path="data/global_data/cost/se_plan/opportunity_cost.tif",
-        unit='2020 USD/ha/yr',
+        data_unit='2020 USD/ha/yr',
         source='https://www.nature.com/articles/s41558-024-02068-1',
         scale=1000,
         band=1,
@@ -65,7 +68,7 @@ class GlobalLayer:
         description="""Implementation cost of restoring tree cover for passive regeneration based on Busch et al. (2024),
         Costs are summed over a period of 30 years and time-discounted at a rate of 5% annually.""",
         gcs_path="data/global_data/cost/se_plan/implementation_cost.tif",
-        unit='2020 USD/ha',
+        data_unit='2020 USD/ha',
         source='https://www.nature.com/articles/s41558-024-02068-1',
         scale=1000,
         band=1,
@@ -78,7 +81,7 @@ class GlobalLayer:
         description="""Implementation cost of restoring tree cover for plantation of native species based on Busch et al. (2024),
         Costs are summed over a period of 30 years and time-discounted at a rate of 5% annually.""",
         gcs_path="data/global_data/cost/se_plan/implementation_cost.tif",
-        unit='2020 USD/ha',
+        data_unit='2020 USD/ha',
         source='https://www.nature.com/articles/s41558-024-02068-1',
         scale=1000,
         band=2,
@@ -91,7 +94,7 @@ class GlobalLayer:
         description="""Implementation cost of restoring tree cover for plantation of introduced species based on Busch et al. (2024),
         Costs are summed over a period of 30 years and time-discounted at a rate of 5% annually.""",
         gcs_path="data/global_data/cost/se_plan/implementation_cost.tif",
-        unit='2020 USD/ha',
+        data_unit='2020 USD/ha',
         source='https://www.nature.com/articles/s41558-024-02068-1',
         scale=1000,
         band=3,
@@ -103,7 +106,7 @@ class GlobalLayer:
         full_name="Recreation, hunting and fishing",
         description="Recreation-related services are the ecosystem contributions, in particular through the biophysical characteristics and qualities of ecosystems, that enable people to use and enjoy the environment through direct, in-situ, physical and experiential interactions with the environment. This includes services to both locals and non-locals (i.e., visitors, including tourists). Recreation-related services may also be supplied to those undertaking recreational fishing and hunting. This is a final ecosystem service.",
         gcs_path="data/global_data/benefit/siikamaki/rec_2020_global_4326.tif",
-        unit='2020 USD/ha',
+        data_unit='2017 USD/ha',
         source='https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099101124145545368',
         scale=10000,
         band=1,
@@ -115,7 +118,7 @@ class GlobalLayer:
         full_name="Non-wood forest products",
         description="Wild animals, plants and other biomass provisioning services are the ecosystem contributions to the growth of wild animals, plants and other biomass that are captured and harvested in uncultivated production contexts by economic units for various uses. The scope includes non-wood forest products (NWFP) and services related to hunting, trapping and bio-prospecting activities; but excludes wild fish and other natural aquatic biomass (included in previous class). This is a final ecosystem service.",
         gcs_path="data/global_data/benefit/siikamaki/nwfp_2020_global_4326.tif",
-        unit='2020 USD/ha',
+        data_unit='2017 USD/ha',
         source='https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099101124145545368',
         scale=10000,
         band=1,
@@ -127,7 +130,7 @@ class GlobalLayer:
         full_name="Forest Water Service Value",
         description="A combination of water related services: Water supply, Water purification services, rainfall pattern regulation services, soil erosion control services, Baseline flow maintenance services, Peak flow mitigation services, and River flood mitigation services",
         gcs_path="data/global_data/benefit/siikamaki/wat_2020_global_4326.tif",
-        unit='2020 USD/ha',
+        data_unit='2017 USD/ha',
         source='https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099101124145545368',
         scale=10000,
         band=1,
@@ -139,34 +142,26 @@ class GlobalLayer:
         full_name="Habitat and species protection",
         description="Ecosystem and species appreciation concerns the wellbeing that people derive from the existence and preservation of the environment for current and future generations, irrespective of any direct or indirect use.",
         gcs_path="data/global_data/benefit/siikamaki/hab_2020_global_4326.tif",
-        unit='2020 USD/ha',
+        data_unit='2017 USD/ha',
         source='https://documents.worldbank.org/en/publication/documents-reports/documentdetail/099101124145545368',
         scale=10000,
         band=1,
         bucket='nbs-tool-public'
     )
 
-    WB_ADMIN1_BOUNDARIES = GlobalLayerData(
-        name="WB_ADMIN1_BOUNDARIES",
-        full_name="Administrative boundaries",
-        description='Official ADMIN 1 World Bank Boundaries (last updated: June 17, 2025)',
-        gcs_path='data/global_data/admin/WorldBankOfficialBoundariesAdmin1.gpkg',
-        unit='n/a',
-        source='https://datacatalog.worldbank.org/search/dataset/0038272/world-bank-official-boundaries',
-        scale=1,
-        band=1,
-        bucket='nbs-tool-public'
-    )
+
 
 
 @dataclass
 class GlobalVectorLayerData( variable_template.Data):
     """Dataclass to store metadata for a variable. This template will be used to create spatial variables"""
-    unit: str
+
     gcs_path: str
     source: str
     layer: str
     bucket: str
+    unit: str = 'USD per ha per year'
+    data_unit: str = None
 
 
     @classmethod
@@ -206,7 +201,7 @@ class GlobalVectorLayer:
         full_name="Mangrove flood benefits",
         description="""This work measures the flood protection service of mangroves all over the world for two climatic conditions: (1) Cyclonic- i.e., the conditions high-intensity extreme waves and storm surge induced by tropical cyclones and (2) Non-cyclonic, i.e., the “regular” waves generated by low-intensity local storms.""",
         gcs_path="/vsigs/nbs-tool-public/data/global_data/benefit/menendez/UCSC_CWON_studyunits.gpkg",
-        unit='2020 USD',
+        data_unit='2020 USD',
         source='https://www.nature.com/articles/s41598-020-61136-6',
         layer='UCSC_CWON_studyunits',
         bucket='nbs-tool-public'
@@ -216,8 +211,8 @@ class GlobalVectorLayer:
         name="WB_ADMIN1_BOUNDARIES",
         full_name="Administrative boundaries",
         description='Official ADMIN 1 World Bank Boundaries (last updated: June 17, 2025)',
-        gcs_path='data/global_data/admin/WorldBankOfficialBoundariesAdmin1.gpkg',
-        unit='n/a',
+        gcs_path='/vsigs/nbs-tool-public/data/global_data/admin/WorldBankOfficialBoundariesAdmin1.gpkg',
+        data_unit='n/a',
         source='https://datacatalog.worldbank.org/search/dataset/0038272/world-bank-official-boundaries',
         layer='WB_GAD_ADM1',
         bucket='nbs-tool-public'
