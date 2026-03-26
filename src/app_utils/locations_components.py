19fcbf48af84a46eca27f890870eff47572291d0
koentieskens <koen.tieskens@gmail.com>
2026-03-26 10:32:28 -0400

    Add ISO3 country code retrieval and storage in `SAVED_COUNTRY_ISO3` session variable

--- src/app_utils/locations_components.py
+++ src/app_utils/locations_components.py
@@ -161,11 +161,12 @@ class LocationManager:
             ssm.SAVED_POLYGON.set(ssm.UNSAVED_POLYGON.get())
             ssm.AOI_GDF.set(gpd.GeoDataFrame([1], geometry=[ssm.DRAWN_POLYGON.get()], crs='EPSG:4326'))
             ssm.ZOOM_LEVEL.set(self._zoom_from_area(ssm.POLYGON_AREA.get()))
-            county, country, country_code = St_Utils.get_location_info(ssm.PROJECT_LOCATION.get()['lat'],
+            county, country, country_code, iso3 = St_Utils.get_location_info(ssm.PROJECT_LOCATION.get()['lat'],
                                                          ssm.PROJECT_LOCATION.get()['lon'])
             ssm.SAVED_COUNTRY.set(country)
             ssm.SAVED_REGION.set(county)
             ssm.SAVED_COUNTRY_CODE.set(country_code)
+            ssm.SAVED_COUNTRY_ISO3.set(iso3)
             st.rerun()
         except TypeError:
             ssm.LOCATION_ACTIVATED.set(False)
@@ -188,11 +189,12 @@ class LocationManager:
             gdf = self.create_circle_gdf(ssm.MANUAL_CENTROID.get()[0], ssm.MANUAL_CENTROID.get()[1], ssm.MANUAL_AREA.get())[0]
             ssm.AOI_GDF.set(gdf)
             ssm.ZOOM_LEVEL.set(self._zoom_from_area(ssm.MANUAL_AREA.get()))
-            county, country, country_code = St_Utils.get_location_info(ssm.PROJECT_LOCATION.get()['lat'],
+            county, country, country_code, iso3 = St_Utils.get_location_info(ssm.PROJECT_LOCATION.get()['lat'],
                                                          ssm.PROJECT_LOCATION.get()['lon'])
             ssm.SAVED_COUNTRY.set(country)
             ssm.SAVED_REGION.set(county)
             ssm.SAVED_COUNTRY_CODE.set(country_code)
+            ssm.SAVED_COUNTRY_ISO3.set(iso3)
             st.rerun()
         except TypeError:
             ssm.LOCATION_ACTIVATED.set(False)
@@ -311,10 +313,11 @@ class LocationManager:
         # Get location information based on coordinates
         if ssm.LOCATION_ACTIVATED.get():
 
-            county, country, country_code = St_Utils.get_location_info(ssm.PROJECT_LOCATION.get()['lat'], ssm.PROJECT_LOCATION.get()['lon'])
+            county, country, country_code, iso3 = St_Utils.get_location_info(ssm.PROJECT_LOCATION.get()['lat'], ssm.PROJECT_LOCATION.get()['lon'])
             ssm.SAVED_COUNTRY.set(country)
             ssm.SAVED_REGION.set(county)
             ssm.SAVED_COUNTRY_CODE.set(country_code)
+            ssm.SAVED_COUNTRY_ISO3.set(iso3)
 
             # Display location information
             location_text = ""
